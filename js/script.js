ea88535cc8a06c6cd014dd9792708eaf7547f72a
henrique <henrique.html87@gmail.com>
2024-06-12 12:32:57 -0300

    mario

---
+++ js/script.js
@@ -0,0 +1,104 @@
+const a = document.querySelector('.mario');
+const cano = document.querySelector('.pipe');
+const nuvem = document.querySelector('.nuvem')
+const audiomorreu = document.querySelector("#audiomorreu")
+const audiomoeda = document.querySelector("#moeda")
+var contador = document.querySelector('.contador')
+var pontos = false;
+var contarMaisUm =0 ;
+var incremendoDeVelocidade = 1000;
+var contarcinco = 0;
+const b = () => // adicionar a função pular a classe mario 
+{
+
+a.classList.add('pular')
+
+setTimeout( ()=>{  a.classList.remove('pular')                  },500 )  // remover a função pular a classe mario +  delay
+
+
+
+
+
+}
+
+const velocidade = (contarcinco) =>
+{
+   if(contarcinco ==5) // numero de pulos = 5
+   {
+    contarcinco= 0;  // zerar contador 
+    incremendoDeVelocidade  = incremendoDeVelocidade - 10;   // variavel que determinar a velocidade do jogo 
+    cano.style.animation = `movercano ${incremendoDeVelocidade }ms infinite linear `  // muda a velocidade da animação do css 
+    
+
+   }
+
+
+}
+
+
+
+const loop = setInterval(()=>
+ {
+   
+    
+    const canoPosicao = cano.offsetLeft; // posição do cano
+    const marioPosicao = +window.getComputedStyle(a).bottom.replace('px',''); // posição do mario com repalce por vazio  + convertendo em numero (+)
+    const nuvemposicao = +window.getComputedStyle(nuvem).right.replace('px',''); // posição do nuvem com repalce por vazio  + convertendo em numero (+)
+
+
+
+    if( canoPosicao <= 120 && canoPosicao>0 && marioPosicao<80)   // se posição do cano for = 120 e o mario nao estiver no ar (altura do pulo = 80)
+    {
+       cano.style.animation = 'none';          //  animação = 0
+       cano.style.left = `${canoPosicao}px`;   // joga o cano na posição atual no loop 
+
+
+       a.style.animation = 'none';             //  animação = 0
+       a.style.bottom = `${marioPosicao}px`;   // joga o mario na posição atual no loop 
+
+       nuvem.style.animation = 'none';          //  animação = 0
+       nuvem.style.right = `${nuvemposicao}px`; // joga a nuvem na posição atual no loop 
+      
+       a.src = "img/game-over.png";     // troca a imagem do mario por imagem de game over 
+       a.style.width = '75px';
+       a.style.marginLeft ='50px';
+       
+       audiomorreu.play();
+
+       clearInterval(loop);
+       
+        }
+
+
+
+        if (canoPosicao< 0 &&pontos == false)    //contador
+        { 
+
+         velocidade(contarcinco)   //função contar 5 canos pulados e incremenda velocidade 
+         contarcinco++;            //quantas vezes pulou
+                       
+            contador.innerHTML = contarMaisUm ; //numero de canos pulados na tela 
+             
+            contarMaisUm++; //soma um cano pulado
+             audiomoeda.playbackRate = 7; // tempo do audio do pulo
+              audiomoeda.play();        // play audio
+              
+              pontos = true;  
+             
+       
+           }
+        if( canoPosicao> 0 && pontos == true)
+
+        
+        {
+
+         pontos =  false
+
+
+        }
+
+} ,10)
+
+
+
+document.addEventListener('keydown',b);  // listener que executa a função b ao ser presionada qualquer tecla
